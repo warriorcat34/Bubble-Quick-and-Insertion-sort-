3882dcb7166eb09ee409434749fbeed9f0e669ee
warriorcat34 <finndips279@gmail.com>
2024-03-07 18:03:19 +0200

    RABOTI VE

---
+++ quicksort/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace quicksort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quick Sort");
+            int[] arr = new int[] { 2, 4, -5, 1, 10 };
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int swapVar = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = swapVar;
+                    }
+                }
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
